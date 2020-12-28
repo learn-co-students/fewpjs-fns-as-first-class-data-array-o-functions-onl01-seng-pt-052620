471d6dcb7976a08564cd53d16c3c933cd1faf582
Capsulecoding <capsulecoding@icloud.com>
2020-12-28 02:50:21 -0800

    Done.

---
+++ index.js
@@ -0,0 +1,45 @@
+function wakeDog(dogName, dogBreed) {
+    let phrase = `Wake ${dogName} the ${dogBreed}`;
+    console.log(phrase);
+    return phrase;
+}
+
+function leashDog(dogName, dogBreed) {
+    let phrase = `Leash ${dogName} the ${dogBreed}`;
+    console.log(phrase);
+    return phrase
+}
+
+function walkToPark(dogName, dogBreed) {
+    let phrase = `Walk to the park with ${dogName} the ${dogBreed}`;
+    console.log(phrase);
+    return phrase
+}
+
+function throwFrisbee(dogName, dogBreed) {
+    let phrase = `Throw the frisbee for ${dogName} the ${dogBreed}`;
+    console.log(phrase);
+    return phrase
+}
+
+function walkHome(dogName, dogBreed) {
+    let phrase = `Walk home with ${dogName} the ${dogBreed}`;
+    console.log(phrase);
+    return phrase
+}
+
+function unleashDog(dogName, dogBreed) {
+    let phrase = `Unleash ${dogName} the ${dogBreed}`;
+    console.log(phrase);
+    return phrase
+}
+
+const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
+
+function exerciseDog(dogName, dogBreed) {
+    let returnValues = []
+    for (let i = 0; i < routine.length; i++) {
+        returnValues.push(routine[i](dogName, dogBreed));
+    }
+    return returnValues
+}
